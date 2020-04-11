bc91e52d9f486310d4a537b2e2669086511badb7
Artem <triple8six@gmail.com>
2020-04-10 23:00:00 -0400

    p

--- src/Easy/treeSumOfElements.java
+++ src/Easy/treeSumOfElements.java
@@ -1,7 +1,12 @@
 package Easy;
 
 public class treeSumOfElements {
-
+	
+	public int sum(TreeNode root) { 
+	    if(root == null) return 0;
+	    return sum(root.right) + sum(root.left) + root.data;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
