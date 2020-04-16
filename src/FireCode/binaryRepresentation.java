fc7fcbd09c453f3ec03ead02faab47c57456fde9
Artem <triple8six@gmail.com>
2020-04-16 18:13:00 -0400

    P

--- src/FireCode/binaryRepresentation.java
+++ src/FireCode/binaryRepresentation.java
@@ -1,9 +1,14 @@
 package FireCode;
 
 public class binaryRepresentation {
-
+	public int binaryRepresentation(int x) {
+		if(x>1) {
+			return 0;
+		}
+		return binaryRepresentation(x/2) + binaryRepresentation(x%2);
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
