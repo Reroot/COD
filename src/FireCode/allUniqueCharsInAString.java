6c37bc4051d746b664569de0a37d82a2cf3c7230
Artem <triple8six@gmail.com>
2020-04-16 23:21:17 -0400

    p

--- src/FireCode/allUniqueCharsInAString.java
+++ src/FireCode/allUniqueCharsInAString.java
@@ -6,7 +6,26 @@ import java.util.Set;
 public class allUniqueCharsInAString {
 	// java.util.* and java.util.streams.* have been imported for this problem.
 	// You don't need any other imports.
+	public static boolean areAllCharactersUnique2(String str){
+	    //256 map
+	    //or real map and iter on a count of 1 get the keyset
+	    if(str == null || str.length() == 0) return true;
+	    if(str.length()==1) return true;
+	    char[] ca = new char[256];
+	    for(int i = 0;i<str.length();i++) {
+	        ca[str.charAt(i)]++;
+	    }
+	    //fill with the count everytime they are hit
+	    for(int i = 0;i<ca.length;i++) { // ca .length becuase it will only set to str length
+	        if(ca[i] > 1) {
+	            return false;
+	        }
+	    }
+	    
+	    return true;
 
+
+	}
 	public static boolean areAllCharactersUnique(String str){
 	        if(str == null) {
 	            return true;
