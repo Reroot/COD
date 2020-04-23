8c479977388676834cb9405936671f1dfe7b0dcb
Artem <triple8six@gmail.com>
2020-04-23 18:30:30 -0400

    p

---
+++ src/DropBox/swapNodesInPairsTest2.java
@@ -0,0 +1,39 @@
+package DropBox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import DropBox.swapNodesInPairs.Node;
+
+class swapNodesInPairsTest2 {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		swapNodesInPairs swapNodesInPairs = new swapNodesInPairs();
+//      ListNode head1 = new ListNode(new int[]{});
+//      System.out.println(swapNodesInPairs.swapPairs(head1));
+
+      ListNode head2 = new ListNode(2);
+      System.out.println(swapNodesInPairs.swapPairs2(head2));
+
+      ListNode head3 = new ListNode(new int[]{1, 2});
+      System.out.println(swapNodesInPairs.swapPairs(head3));
+
+      ListNode head4 = new ListNode(new int[]{1, 2, 3});
+      System.out.println(swapNodesInPairs.swapPairs(head4));
+
+      ListNode head5 = new ListNode(new int[]{1, 2, 3, 4});
+      System.out.println(swapNodesInPairs.swapPairs(head5));
+
+      ListNode head6 = new ListNode(new int[]{1, 2, 3, 4, 5});
+      System.out.println(swapNodesInPairs.swapPairs(head6));
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
