f27c2add1e1601248a0eb8f7bc3bf35d57a8c981
Artem <triple8six@gmail.com>
2020-04-23 21:59:28 -0400

    p

--- src/FireCode/computeBinary.java
+++ src/FireCode/computeBinary.java
@@ -1,6 +1,14 @@
 package FireCode;
 
 public class computeBinary {
+	public static String computeBinary(int val) {
+	    // ANYNUMBER = val/2+val%2, broken down by this formula 
+	    if(val < 2) {
+	        return Integer.toString(val);
+	    } else {
+	        return computeBinary(val/2) + computeBinary(val%2);
+	    }
+	}
 //	Write a method to compute the binary representation of a positive integer. The method should return a string with 1s and 0s.
 //
 //			computeBinary(6) ==> "110"
---
+++ src/FireCode/mergeTwoSortedArrays.java
@@ -0,0 +1,35 @@
+package FireCode;
+
+public class mergeTwoSortedArrays {
+	public static int[] merge(int[] arrLeft, int[] arrRight){
+		int[] merged = new int[arrLeft.length+arrRight.length];
+		int lenRight = arrRight.length;
+		int lenLeft = arrLeft.length;
+		//3 indicies 
+		int indleft = 0, indRight = 0, indMerged = 0;
+		//sort the sorted by leftmost
+		while(indleft < lenLeft && indRight < indRight) {//fill the array
+			if(arrLeft[indleft++] < arrRight[indRight++]) {
+				merged[indleft++] = arrLeft[indleft++];
+			} else {
+				merged[indleft++] = arrRight[indRight++];
+			}
+		}
+		while(indleft < lenLeft) {
+			merged[indleft] = arrLeft[lenLeft];
+		}
+		while(indRight < lenRight) {
+			merged[indleft] = arrLeft[lenLeft];
+		}
+		
+		
+		return merged;
+	}
+
+	public static void main(String[] args) {
+		// rec
+		
+
+	}
+
+}
