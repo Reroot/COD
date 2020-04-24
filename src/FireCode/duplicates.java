2438f923f6f86f1559907cf22bc8a046a1eb7df9
Artem <triple8six@gmail.com>
2020-04-24 01:48:01 -0400

    p

--- src/FireCode/duplicates.java
+++ src/FireCode/duplicates.java
@@ -1,6 +1,7 @@
 package FireCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeMap;
 //Write a method duplicate to find the repeated or duplicate elements in an array.
@@ -16,10 +17,26 @@ import java.util.TreeMap;
 //Note: You may use toString() method to return the
 //standard string representation of most data structures, and
 //Arrays.sort() to sort your result.
-public class ReapeatedElelentsInAnArray {
+public class duplicates {
 //	Runtime Complexity
 //	O(n log(n))
+	public static String duplicate2(int[] numbers){
+	    ArrayList<Integer> setList = new ArrayList<Integer>();
+	    HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+	    if(numbers.length == 0) return setList.toString();
+	    int count  = 0;
+	    for(int i = 0;i<numbers.length;i++) {
+	        m.put(numbers[i], m.getOrDefault(numbers[i], 0)+1);
+	    }
+	    Set<Integer> setK = m.keySet();
 
+	    for(Integer s : setK) {
+	        if(m.get(s) > 1) {
+	            setList.add(s);
+	        }
+	    }
+	    return setList.toString();
+	}
 	public static String duplicate(int[] numbers){
 	 //we should use treemap for this as treemap sorts, as we put elements int
 	 //the iterate and filter the treemap  
